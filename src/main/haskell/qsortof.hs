0568a8f32cff59116fbe39c5998588c984a2f3c8
vwulf <smrvis20@gmail.com>
2025-01-13 00:06:14 -0800

    Fix qsort to be 1 pass and ran experiment to take min k
    ghci> take 3 $ qsort $ Data.List.unfoldr (\b -> if b == 0 then Nothing else Just(b, b-1)) 100000
    [1,2,3]
    still works at reasonable pace compared to full sort
    ghci> qsort $ Data.List.unfoldr (\b -> if b == 0 then Nothing else Just(b, b-1)) 100000

--- src/main/haskell/qsortof.hs
+++ src/main/haskell/qsortof.hs
@@ -7,13 +7,15 @@ qsort :: (Ord a) => [a] -> [a]
 qsort [] = []
 qsort ns = (qsort s) ++ m ++ (qsort l) where
   x = head ns
-  s = [n | n <- ns, n < x]
-  l = [n | n <- ns, n > x]
-  m = [n | n <- ns, n == x]
+  (s, m, l) = foldl (\(s, m, l) e ->
+                  if e < x then (e:s, m, l)
+                  else if e == x then (s, e:m, l) 
+                  else (s, m, e:l)) ([],[],[]) ns
 
 main = do
+  putStrLn $ "Enter List of integers in [p,q,r,] format"
   inputs <- getContents
   forM_ (lines inputs) $ \aline -> do
-  let input = aline
-  let y :: [Int] = read input
-  putStrLn $ "Sort of " ++ input ++ " is " ++ show(qsort y)
+    let input = aline
+    let y :: [Int] = read input
+    putStrLn $ "Sort of " ++ input ++ " is " ++ show(qsort y)
