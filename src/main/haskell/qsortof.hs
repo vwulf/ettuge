aff0c06105c3c2dfc1296f8159f188064b42f7a6
vwulf <smrvis20@gmail.com>
2025-01-14 22:33:13 -0800

    Changed to foldl'
    Added tests using unfold wrapper. Still need to fix print which is
    coercing types to float.

--- src/main/haskell/qsortof.hs
+++ src/main/haskell/qsortof.hs
@@ -1,22 +1,60 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Monad
-import Data.Monoid
-import Data.Typeable
+import Control.Monad ( forM_ )
+import Data.Monoid ()
+import Data.Typeable ()
+import Data.List ( foldl', unfoldr )
+import GHC.Float (sqrtDouble, sqrtFloat)
 
 qsort :: (Ord a) => [a] -> [a]
 qsort [] = []
-qsort ns = qsort s ++ m ++ qsort l where
-  x = head ns
-  empty = mempty :: [a]
-  emptysml = (empty, empty, empty)
-  (s, m, l) = foldl (\(s, m, l) e ->
-                  if e < x then (e:s, m, l)
-                  else if e == x then (s, e:m, l)
-                  else (s, m, e:l)) emptysml ns
+qsort elems = qsort lt ++ eq ++ qsort gt where
+  pivot = head elems
+  emptysml = (mempty, mempty, mempty)
+  (lt, eq, gt) = foldl' (\(lt, eq, gt) elem ->
+                  if elem < pivot
+                    then (elem:lt, eq, gt)
+                  else if elem == pivot
+                    then (lt, elem:eq, gt)
+                  else
+                    (lt, eq, elem:gt)
+                  ) emptysml elems
 
+tillcount :: (Ord a) => (a -> a -> Bool) ->
+                    a ->
+                    (a -> a -> a) ->
+                    a ->
+                    (a -> b) ->
+                    a ->
+                    Maybe (b, a)
+tillcount reaches end step init f counter =
+  if counter `reaches` end
+  then Nothing
+  else Just(f counter, counter `step` init)
+
+unless :: (Ord a) => a ->
+                     (a -> a -> Bool) ->
+                     a ->
+                     (a -> a -> a) ->
+                     a ->
+                     (a -> b) ->
+                     [b]
+unless counter reaches end step init f =
+  unfoldr (tillcount reaches end step init f) counter
+
+main :: IO ()
 main = do
-  putStrLn $ "Enter List of integers in [p,q,r,] format"
+  let t1 = unless 0 (==) 10 (+) 1 id -- [0,1,2,3,4,5,6,7,8,9,10]
+  let t2 = unless 0 (==) 10 (+) 1 (*2) -- [0,2,4,6,8,10]
+  let t3 = unless 10 (>=) 0 (-) 1 (*2) -- []
+  let t4 = unless 10 (<) 0 (-) 1 (*2) -- [20,18,16,14,12,10,8,6,4,2,0]
+  let t5 = unless (10.0^^20) (<) (sqrtFloat 2.0) (\x _ -> sqrtFloat x) 1 (*1) -- [1.4330126,2.053525,4.2169647,17.782793,316.22775,100000.0,1.0e10,1.0e20]
+  let t6 = unless (10.0^^20) (<) 1.1 (\x _ -> sqrtFloat x) 1 (*1) -- [1.197085,1.4330126,2.053525,4.2169647,17.782793,316.22775,100000.0,1.0e10,1.0e20]
+
+  forM_ [t1, t2, t3, t4, t5, t6] $ \t -> do
+    putStrLn $ "Sort of " ++ show t ++ " is " ++ show (qsort t)
+
+  putStrLn "Enter List of integers in [p,q,r,] format"
   inputs <- getContents
   forM_ (lines inputs) $ \aline -> do
     let input = aline
