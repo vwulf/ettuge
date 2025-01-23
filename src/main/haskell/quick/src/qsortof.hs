238c8336306816e472008753e149cf49c6381e2c
vwulf <smrvis20@gmail.com>
2025-01-22 23:53:33 -0800

    Added momentum pivot
    Refactored to use . syntax for records.
    
    Test:
    
    :set +s
    :set +m
    import Qsortof
    import System.Random
    pureGen = mkStdGen 137
    rs = take 100 $ randomRs (10, 10000) pureGen
    take 20 $ qsort rs
    qsel rs 20
    
    ghci> take 20 $ qsort rs
    [69,117,229,250,373,419,480,526,537,795,820,1035,1138,1200,1413,1589,1775,1960,1987,2040]
    (0.01 secs, 700,752 bytes)
    ghci> qsel rs 20
    "pivot: Just 1035 x: 1035, pivotg: Just 3091, k: 20, off: 0, ltc: 11, eqc: 1, gtc: 88"
    "pivot: Just 3091 x: 6281, pivotl: Just 3361, k: 20, off: 12, ltc: 17, eqc: 0, gtc: 71"
    "pivot: Just 3361 x: 1138, pivotl: Just 1138, k: 20, off: 12, ltc: 17, eqc: 0, gtc: 0"
    "pivot: Just 1138 x: 1138, pivotg: Just 2114, k: 20, off: 12, ltc: 0, eqc: 1, gtc: 16"
    "pivot: Just 2114 x: 2785, pivotl: Just 3600, k: 20, off: 13, ltc: 7, eqc: 0, gtc: 9"
    "pivot: Just 3600 x: 1413, pivotl: Just 1413, k: 20, off: 13, ltc: 7, eqc: 0, gtc: 0"
    "pivot: Just 1413 x: 1413, pivotg: Just 2220, k: 20, off: 13, ltc: 1, eqc: 1, gtc: 5"
    "pivot: Just 2220 x: 1960, pivotl: Just 3393, k: 20, off: 15, ltc: 5, eqc: 0, gtc: 0"
    "pivot: Just 3393 x: 1960, pivotl: Just 1960, k: 20, off: 15, ltc: 5, eqc: 0, gtc: 0"
    "pivot: Just 1960 x: 1960, pivotg: Just 2119, k: 20, off: 15, ltc: 2, eqc: 1, gtc: 2"
    "pivot: Just 2119 x: 2040, pivotl: Just 2517, k: 20, off: 18, ltc: 2, eqc: 0, gtc: 0"
    "pivot: Just 2517 x: 2040, pivotl: Just 2040, k: 20, off: 18, ltc: 2, eqc: 0, gtc: 0"
    "pivot: Just 2040, x: 2040, k:20, off: 18, ltc: 1, eqc: 1, gtc: 0"
    Just (2040,[1035,1138,373,419,795,69,1413,820,117,1960,480,229,537,1589,2040,1200,1775,1987,526,250])
    (0.01 secs, 1,420,288 bytes)

--- src/main/haskell/quick/src/qsortof.hs
+++ src/main/haskell/quick/src/qsortof.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 module Qsortof where
 import Data.Monoid ()
 import Data.Typeable ()
@@ -9,7 +10,6 @@ import Data.Bifunctor (Bifunctor(first), Bifunctor(second), Bifunctor (bimap))
 import Data.Biapplicative (Biapplicative (bipure), Biapplicative ((<<*>>)))
 import Debug.Trace (traceShow)
 import Data.Maybe (listToMaybe, fromMaybe)
-import Data.Bifunctor.Biff (Biff)
 
 
 qsort :: (Ord a) => [a] -> [a]
@@ -108,15 +108,15 @@ data H a b = H {
 instance (Ord a, Integral a, Num b, Ord b) => Semigroup (H a b) where
   (<>) :: H a b -> H a b -> H a b
   h1 <> h2 = let
-    h_min_ = maybeOp min (h_min h1) (h_min h2)
-    h_max_ = maybeOp max (h_max h1) (h_max h2)
+    h_min_ = maybeOp min h1.h_min h2.h_min
+    h_max_ = maybeOp max h1.h_max h2.h_max
     in H {
-      h_lst = h_lst h1 ++ h_lst h2,
-      h_cnt = h_cnt h1 + h_cnt h2,
+      h_lst = h1.h_lst ++ h2.h_lst,
+      h_cnt = h1.h_cnt + h2.h_cnt,
       h_min = h_min_,
       h_max = h_max_,
-      h_wmid = calcWmid h_min_ h_max_ (h_wmid h1) (h_wmid h2),
-      h_sum = fmap (+) (h_sum h1) <*> h_sum h2
+      h_wmid = calcWmid h_min_ h_max_ h1.h_wmid h2.h_wmid,
+      h_sum = fmap (+) h1.h_sum <*> h2.h_sum
     }
 
 instance (Integral a, Num b, Ord b) => Monoid (H a b) where
@@ -132,24 +132,23 @@ instance (Integral a, Num b, Ord b) => Monoid (H a b) where
 instance Functor (H a) where
   fmap :: (b -> c) -> H a b -> H a c
   fmap f h = H {
-    h_lst = h_lst h,
-    h_cnt = f (h_cnt h),
-    h_min = h_min h,
-    h_max = h_max h,
-    h_wmid = h_wmid h,
-    h_sum = h_sum h
+    h_lst = h.h_lst,
+    h_cnt = f h.h_cnt,
+    h_min = h.h_min,
+    h_max = h.h_max,
+    h_wmid = h.h_wmid,
+    h_sum = h.h_sum
   }
 
 instance Bifunctor H where
   bimap :: (a -> c) -> (b -> d) -> H a b -> H c d
-  bimap f g h =
-    H {
-    h_lst = map f (h_lst h),
-    h_cnt = g (h_cnt h),
-    h_min = fmap f (h_min h),
-    h_max = fmap f (h_max h),
-    h_wmid = fmap f (h_wmid h),
-    h_sum = fmap f (h_sum h)
+  bimap f g h = H {
+    h_lst = map f h.h_lst,
+    h_cnt = g h.h_cnt,
+    h_min = fmap f h.h_min,
+    h_max = fmap f h.h_max,
+    h_wmid = fmap f h.h_wmid,
+    h_sum = fmap f h.h_sum
   }
 
 instance Biapplicative H where
@@ -165,16 +164,16 @@ instance Biapplicative H where
 
   (<<*>>) :: H (a -> c) (b -> d) -> H a b -> H c d
   (<<*>>) h1 h2 = let
-    h_min_ = h_min h1 <*> h_min h2
-    h_max_ = h_max h1 <*> h_max h2
-    h_wmid_ = h_wmid h1 <*> h_wmid h2
+    h_min_ = h1.h_min <*> h2.h_min
+    h_max_ = h1.h_max <*> h2.h_max
+    h_wmid_ = h1.h_wmid <*> h2.h_wmid
     in H {
-      h_lst = zipWith ($) (h_lst h1) (h_lst h2),
-      h_cnt = h_cnt h1 (h_cnt h2),
+      h_lst = zipWith ($) h1.h_lst h2.h_lst,
+      h_cnt = h1.h_cnt h2.h_cnt,
       h_min = h_min_,
       h_max = h_max_,
       h_wmid = h_wmid_,
-      h_sum = h_sum h1 <*> h_sum h2
+      h_sum = h1.h_sum <*> h2.h_sum
     }
 
 maybeOp :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
@@ -193,7 +192,8 @@ fromMaybeOp :: (a -> a -> a) -> Maybe a -> a -> a
 fromMaybeOp f Nothing x = x
 fromMaybeOp f (Just y) x = f y x
 
-calcWmid :: (Num a, Ord a, Integral a) => Maybe a -> Maybe a -> Maybe a -> Maybe a -> Maybe a
+calcWmid :: (Num a, Ord a, Integral a) =>
+  Maybe a -> Maybe a -> Maybe a -> Maybe a -> Maybe a
 calcWmid hmin hmax hwmid elem = let
   calcWmid' :: (Num a, Ord a, Integral a) => a -> a -> a -> a -> a
   calcWmid' min' max' wmid' elem = let
@@ -210,56 +210,110 @@ calcWmid hmin hmax hwmid elem = let
 
 calcH :: (Num a, Ord a, Integral a, Num b) => a -> H a b -> H a b
 calcH elem h = let
-  h_min_ = maybeOp min (h_min h) (Just elem)
-  h_max_ = maybeOp max (h_max h) (Just elem)
+  h_min_ = maybeOp min h.h_min $ Just elem
+  h_max_ = maybeOp max h.h_max $ Just elem
   in H {
-  h_lst = elem : h_lst h,
-  h_cnt = 1 + h_cnt h,
-  h_min = h_min_,
-  h_max = h_max_,
-  h_wmid = calcWmid h_min_ h_max_ (h_wmid h) (Just elem),
-  h_sum = maybeOp (+) (Just elem) (h_sum h)
-}
+    h_lst = elem : h.h_lst,
+    h_cnt = 1 + h.h_cnt,
+    h_min = h_min_,
+    h_max = h_max_,
+    h_wmid = calcWmid h_min_ h_max_ h.h_wmid $ Just elem,
+    h_sum = maybeOp (+) (Just elem) h.h_sum
+  }
+
+pivotnk :: (Integral a) => 
+            H a a ->
+            a ->
+            a ->
+            a ->
+            (a -> a -> a -> a -> a -> a) ->
+            Maybe a
+pivotnk h k off defp form =
+  do
+    min' <- h.h_min
+    max' <- h.h_max
+    let cnt' = h.h_cnt
+    let pivot' = form min' max' cnt' k off
+    if pivot' == defp then
+      listToMaybe h.h_lst
+    else
+      return pivot'
+ 
 
 qsel :: (Show a, Ord a, Integral a) =>
-  [a] -> a -> a
-qsel elems k = qsel' elems k 0 (head elems) where
+  [a] -> a -> Maybe (a, [a])
+qsel elems k = ret where
+  ret
+    | k < 1 = Nothing
+    | k >= fromIntegral (length elems) = Just (maximum elems, elems)
+    | otherwise = do
+            mink <- qsel' elems k 0 (listToMaybe elems)
+            let minkelems = take
+                  (fromIntegral k)
+                  $ filter (<= fromIntegral mink)
+                  elems
+            return (mink, minkelems)
+
+  accf :: (Ord a, Integral a) =>
+   a -> a -> (H a a, H a a, H a a)
+   -> (H a a, H a a, H a a)
+  accf pivot elem acc = let (lt, eq, gt) = acc
+    in
+      if elem < pivot then
+        (calcH elem lt, eq, gt)
+      else
+         if elem == pivot then
+           (lt, calcH elem eq, gt)
+         else
+           (lt, eq, calcH elem gt)
+ 
+  emptysml :: (Integral a) => (H a a, H a a, H a a)
+  emptysml = (mempty, mempty, mempty)
+
   qsel' :: (Show a, Ord a, Integral a) =>
-    [a] -> a -> a -> a -> a
-  qsel' [] k off pivot = pivot
+    [a] -> a -> a -> Maybe a -> Maybe a
+  qsel' [] k off pivot =
+     traceShow (concat ["pivot: ", show pivot, " k: ", show k,
+                ", off: ", show off, " el: ", show (k + off)])
+     Nothing
   qsel' (x:xs) k off pivot
-    | k < off + ltc =
-      traceShow (concat ["pivot: ", show pivot, ", pivotl: ", show pivotl, ", k: ", show k, ", off: ", show off, ", ltc: ", show ltc, ", eqc: ", show eqc, ", gtc: ", show gtc])
-      qsel' ltl k off pivotl
-    | k > off + ltc + eqc =
-      traceShow (concat ["pivot: ", show pivot,", pivotg: ", show pivotg, ", k: ", show k, ", off: ", show off, ", ltc: ", show ltc, ", eqc: ", show eqc, ", gtc: ", show gtc])
-      qsel' gtl k (off + ltc + eqc) pivotg
+    | k <= off + lt.h_cnt =
+      traceShow (concat ["pivot: ", show pivot, " x: ", show x,
+                 ", pivotl: ", show pivotl, ", k: ", show k,
+                 ", off: ", show off, ", lt.h_cnt: ", show lt.h_cnt,
+                 ", eq.h_cnt: ", show eq.h_cnt,
+                 ", gt.h_cnt: ", show gt.h_cnt])
+      qsel' lt.h_lst k off pivotl
+    | k > off + lt.h_cnt + eq.h_cnt =
+      traceShow (concat ["pivot: ", show pivot, " x: ", show x,
+                 ", pivotg: ", show pivotg, ", k: ", show k,
+                 ", off: ", show off, ", lt.h_cnt: ", show lt.h_cnt,
+                 ", eq.h_cnt: ", show eq.h_cnt,
+                 ", gt.h_cnt: ", show gt.h_cnt])
+      qsel' gt.h_lst k (off + lt.h_cnt + eq.h_cnt) pivotg
     |
-      traceShow (concat ["pivot: ", show pivot, ", k: ", show k, ", off: ", show off, ", ltc: ", show ltc, ", eqc: ", show eqc, ", gtc: ", show gtc])
-      otherwise = pivota
+      traceShow (concat ["pivot: ", show pivot, ", x: ", show x,
+                  ", k:", show k, ", off: ", show off,
+                  ", lt.h_cnt: ", show lt.h_cnt,
+                  ", eq.h_cnt: ", show eq.h_cnt,
+                  ", gt.h_cnt: ", show gt.h_cnt])
+      otherwise = maybeOp const pivot (Just x)
     where
-      pivota = pivot
-      emptysml = (mempty, mempty, mempty)
-      (lt, eq, gt) =
-        foldr
-          ( \elem (lt, eq, gt) ->
-               if elem < pivot then
-                 (calcH elem lt, eq, gt)
-               else
-                  if elem == pivot then
-                    (lt, calcH elem eq, gt)
-                  else
-                    (lt, eq, calcH elem gt)
-          ) emptysml (x : xs)
-      (ltl, eql, gtl) = (h_lst lt, h_lst eq, h_lst gt)
-      (ltc, eqc, gtc) = (h_cnt lt, h_cnt eq, h_cnt gt)
-      --pivotl = fromMaybeOp const (h_wmid lt) (head ltl)
-      --pivotg = fromMaybeOp const (h_wmid gt) (head gtl)
-      --lm = fmap (div (h_cnt lt)) (h_sum lt) 
-      lm = maybeOp' div (h_sum lt) $ Just (h_cnt lt)
-      rm = maybeOp' div (h_sum gt) $ Just (h_cnt gt)
-      pivotl = fromMaybeOp const lm (head ltl)
-      pivotg = fromMaybeOp const rm (head gtl)
+      pivota = fromMaybeOp const pivot x
+      (lt, eq, gt) = foldr (accf pivota) emptysml (x:xs)
+      --pivotl = maybeOp const lt.h_wmid (listToMaybe lt.h_lst)
+      --pivotg = maybeOp const gt.h_wmid (listToMaybe gt.h_lst)
+      --lm = maybeOp' div lt.h_sum $ Just lt.h_cnt
+      --rm = maybeOp' div gt.h_sum $ Just gt.h_cnt
+      --pivotl = maybeOp const lm (listToMaybe lt.h_lst)
+      --pivotg = maybeOp const rm (listToMaybe gt.h_lst)
+      pivotl = pivotnk lt k off pivota
+                (\min' max' cnt' k off ->
+                  min' + (k * (max' - min')) `div` cnt')
+      pivotg = pivotnk gt k off pivota
+                (\min' max' cnt' k off ->
+                  min' + ((k - off) * (max' - min')) `div` cnt')
+
 
 
 
